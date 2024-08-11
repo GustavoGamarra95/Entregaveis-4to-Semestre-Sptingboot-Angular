5f6e35a11d97bc596c00770d5bd954478ef764b6
Gustavo Gamarra <gugaro8@gmail.com>
2024-08-11 00:28:00 -0400

    first commit

---
+++ Street Clothing/src/main/java/com/example/Street/Clothing/StreetClothingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Street.Clothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetClothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetClothingApplication.class, args);
+	}
+
+}
---
+++ Street Clothing/src/test/java/com/example/Street/Clothing/StreetClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Street.Clothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
