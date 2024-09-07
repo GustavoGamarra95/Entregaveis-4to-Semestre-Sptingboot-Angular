0a91e029e1c7de5ffcf51221b6d044b59f3f6a91
Gustavo Gamarra <gugaro8@gmail.com>
2024-09-06 21:14:04 -0400

    project added

---
+++ SelfCare/src/test/java/com/example/SelfCare/SelfCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SelfCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
